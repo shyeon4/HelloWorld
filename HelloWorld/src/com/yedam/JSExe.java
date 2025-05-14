9833975077ef3190d3f8a0174e8fb11a1478008b
shyeon4 <shyeon4@naver.com>
2025-05-14 12:35:00 +0900

    복습

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -2,7 +2,7 @@ package com.yedam;
 
 public class JSExe {
 	public static void main(String[] args) {
-		// 1~10 사이의 임의의 값 생성
+		// 1 ~ 10 사이의 임의의 값 생성
 		int sum = 0;
 		for (int i = 1; i <= 5; i++) {
 			int result = (int) (Math.random() * 10) + 1; // 1<= x < 11
@@ -14,7 +14,7 @@ public class JSExe {
 
 	public static void test() {
 		int sum = 0;
-		// 1~ 10까지의 누적
+		// 1 ~ 10까지의 누적
 		for (int i = 1; i <= 10; i++) {
 			if (i % 2 == 1) {
 				sum += i;
