8c9107ca45b9b4f92e405a0b8331694e66c0cc83
shyeon4 <shyeon4@naver.com>
2025-05-14 12:58:10 +0900

    충돌 해결

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -2,25 +2,23 @@ package com.yedam;
 
 public class JSExe {
 	public static void main(String[] args) {
-		// 1 ~ 10 사이의 임의의 값 생성
-		// 변경...
+		// 1 ~ 100 사이의 임의의 값 생성
+		// 깃 충돌로 변경
 		test2();
-		// 충동작성.
 	} // end of main
 
 	public static void test2() {
 		int sum = 0;
 		for (int i = 1; i <= 5; i++) {
-			int result = (int) (Math.random() * 10) + 1; // 1<= x < 11
+			int result = (int) (Math.random() * 100) + 1; // 1<= x < 11
 			sum += result;
 		}
 		System.out.println("결과 : " + sum);
-		// 변경.
 	} // end of main
 
 	public static void test() {
 		int sum = 0;
-		// 1~10까지의 누적
+		// 1 ~ 10까지의 누적
 		for (int i = 1; i <= 10; i++) {
 			if (i % 2 == 1) {
 				sum += i;
