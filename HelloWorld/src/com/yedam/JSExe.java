9a3f25e2784ba06224fa68d266607e6abb64b4cc
shyeon4 <shyeon4@naver.com>
2025-05-14 12:39:46 +0900

    충돌

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -3,14 +3,16 @@ package com.yedam;
 public class JSExe {
 	public static void main(String[] args) {
 		// 1 ~ 10 사이의 임의의 값 생성
+		test2();
+	} // end of main
+	public static void test2() {
 		int sum = 0;
 		for (int i = 1; i <= 5; i++) {
 			int result = (int) (Math.random() * 10) + 1; // 1<= x < 11
 			sum += result;
 		}
 		System.out.println("결과 : " + sum);
-
-	} // end of main
+	}
 
 	public static void test() {
 		int sum = 0;
