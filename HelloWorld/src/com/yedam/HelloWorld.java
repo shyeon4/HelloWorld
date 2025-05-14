8b869bcb6c8ca95f46eb1ea5e464a91356d545c3
shyeon4 <shyeon4@naver.com>
2025-05-14 10:47:22 +0900

    첫 번째 릴리즈

---
+++ HelloWorld/src/com/yedam/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.yedam;
+// 자바는 컴파일(***.java를 실행하는 게 아니고 ***.class로 실행됨)
+
+public class HelloWorld {
+	// main 메소드
+	public static void main(String[] args) {
+		for (int i = 1; i <=3; i++)
+			System.out.println("Hello, World");
+	}
+}
+// public static void 그냥 이런 게 있다 생각하고 넘어가고.. 
