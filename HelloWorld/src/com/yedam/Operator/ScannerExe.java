fddb99ef16973f5fff99a96792c62b141c2e9b32
shyeon4 <shyeon4@naver.com>
2025-05-15 20:34:21 +0900

    Update ScannerExe.java

--- HelloWorld/src/com/yedam/Operator/ScannerExe.java
+++ HelloWorld/src/com/yedam/Operator/ScannerExe.java
@@ -10,30 +10,32 @@ public class ScannerExe {
 		int balance = 0; // 계좌의 금액
 		while(true) {
 			System.out.println("1.입금 2.출금 3.잔액 4.종료");
-			int menu = Integer.parseInt(scn.nextLine()); // Integer.parseInt("30"); 뭔 소리야
+			int menu = Integer.parseInt(scn.nextLine()); // Integer.parseInt("30");
+			int money = 0; // 입출금 처리를 위한 변수
 			if(menu == 1) {
 				System.out.println("입금액을 입력하세요");
 				// 입금 기능 구현
-				int deposit = Integer.parseInt(scn.nextLine());
-				if(balance < 100000) {
-					System.out.println("10만 원을 초과했습니다");
-					return;
-				} else
-					balance += deposit;
+				money = Integer.parseInt(scn.nextLine());
+				if(balance + money > 100000) {
+					System.out.println("10만 원을 초과해서 입금할 수 없습니다");
+				} else {
+					balance = balance + money;
 					System.out.println("입금 완료");
-					
+				}
+										
 			} else if (menu == 2) {
 				System.out.println("출금액을 입력하세요");
 				// 출금 기능 구현
-				int withdraw = Integer.parseInt(scn.nextLine());
-				if(balance - withdraw >= 0) {
-				balance -= withdraw;
-				System.out.println("출금 완료");
-				} else
-					System.out.println("잔액이 부족합니다");
+				money = Integer.parseInt(scn.nextLine());
+				if (balance < money) { // 금액이 10만 원을 초과했을 경우
+					System.out.println("10만 원을 초과해서 출금할 수 없습니다");
+				} else {
+					balance = balance - money;
+					System.out.println("출금 완료");
+				}
 				
 			} else if (menu == 3) {
-				System.out.println("남은 잔액 :" + balance);
+				System.out.println("남은 잔액 : " + balance + "원");
 				
 			} else if (menu == 4) {
 				System.out.println("프로그램을 종료합니다");
